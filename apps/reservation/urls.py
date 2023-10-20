89034f3faa4dfaa69903571ffc9048f17a5ab38f
Lucas <lucasoliveirars22@gmail.com>
2023-10-20 15:37:27 -0300

    form deferir reserva

--- apps/reservation/urls.py
+++ apps/reservation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GrantsView, RoomsView
+from .views import GrantsView, RoomsView, GrantReservationFormView
 
 app_name = "reservation"
 
 urlpatterns = [
     path('grants/', GrantsView.as_view(), name='grants'),
     path('rooms/', RoomsView.as_view(), name='rooms'),
+    path('grant-reservation/', GrantReservationFormView.as_view(), name='grant-reservation'),
 ]
--- apps/reservation/views/__init__.py
+++ apps/reservation/views/__init__.py
@@ -1,7 +1,9 @@
 from .grants import GrantsView
 from .rooms import RoomsView
+from .grant_reservation import GrantReservationFormView
 
 __all__ = [
     GrantsView,
-    RoomsView
+    RoomsView,
+    GrantReservationFormView
 ]
