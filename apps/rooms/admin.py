178c8d9cd372b01124a855400432cb937c19b943
dfbrasil <danieldfb@gmail.com>
2023-12-25 19:35:52 -0300

    last updates

--- apps/rooms/admin.py
+++ apps/rooms/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from .models import Room
 
-from apps.rooms.models import Room
+class RoomAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "room_name",
+        "key",
+        "department",
+        "available",
+        "coordinators_list",  # Esta será uma função personalizada para exibir coordenadores
+    ]
+    list_filter = [
+        "department",
+        "available",
+    ]
 
-admin.site.register(Room)
+    def coordinators_list(self, obj):
+        return ", ".join([coordinator.name for coordinator in obj.id_user_coordinator.all()])
 
+    coordinators_list.short_description = "Avaliadores de Agendamentos"
+
+admin.site.register(Room, RoomAdmin)
